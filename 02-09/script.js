ff1bc9026710b3939f6f028e182831237ca52100
noty-mn <71215517+noty-mn@users.noreply.github.com>
2022-02-14 10:34:31 +0800

    asd

---
+++ 02-09/script.js
@@ -0,0 +1,22 @@
+// {} - scope, (block)
+
+//if else 
+
+//var x = +prompt("too oruulna uu")
+//if(x < 10){
+    // true bh uyd ene dotorh code ajillana 
+//    var isTrue = true;
+//} else {
+    // false ur dun garsan uyd ene heseg ajillana 
+//    isTrue = false; 
+//} 
+
+//console.log(isTrue)
+
+var x = +prompt("too oruulna")
+if(x) {
+    var asd = true; 
+} else {
+    asd = false;
+}
+console.log(asd);
